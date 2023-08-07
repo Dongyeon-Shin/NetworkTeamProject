e626284ec2a47a6320cfb62c75ed8c9c607a1f49
Pass1948 <dldudtls100@naver.com>
2023-08-07 17:42:48 +0900

    feat : 플레이어 애니메이션 추가
    달리기 추가

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -8,6 +8,7 @@ public class PlayerMove : MonoBehaviourPun, IExplosiveReactivable
     private SpeedItem speedItem;
     private Rigidbody rb;
     private PlayerInput playerInput;
+    private Animator animator;
 
     private Vector3 moveDir;
 
@@ -19,6 +20,7 @@ public class PlayerMove : MonoBehaviourPun, IExplosiveReactivable
         stat = GetComponent<TestStat>();
         rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
         if (!photonView.IsMine)
             Destroy(playerInput);
     }
@@ -42,12 +44,15 @@ public class PlayerMove : MonoBehaviourPun, IExplosiveReactivable
 
     void Move()
     {
+        curSpeed = stat.Speed;
         Vector3 vecFor = new Vector3(moveDir.x, 0, moveDir.z).normalized;
         Vector3 vecRb = rb.position;
 
         rb.MovePosition(vecRb + vecFor * 5 * Time.deltaTime);
         if (moveDir.sqrMagnitude >= 0.01f)
             transform.rotation = Quaternion.LookRotation(moveDir);
+
+
     }
 
     public void OnSetting(InputValue value)
@@ -55,10 +60,29 @@ public class PlayerMove : MonoBehaviourPun, IExplosiveReactivable
         // GameManager.UI.ShowInGameUI<InGameUI>("");   // 이후 추가
     }
 
+    public void OnChatting() 
+    {
+        
+    }
+
     public void OnMove(InputValue value)
     {
         moveDir.x = value.Get<Vector2>().x;
         moveDir.z = value.Get<Vector2>().y;
+        if (moveDir.x > 0 || moveDir.z > 0 || moveDir.x < 0 || moveDir.z < 0)
+        {
+            animator.SetBool("Move", true);
+        }
+        
+        else if (moveDir.x == 0 && moveDir.z == 0)
+        {
+            animator.SetBool("Move", false);
+        }
+
+        else
+        {
+            animator.SetBool("Move", false);
+        }
     }
 
     private void OnTriggerStay(Collider other)
