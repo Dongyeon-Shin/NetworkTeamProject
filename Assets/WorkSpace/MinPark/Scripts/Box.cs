5ec764416ee8261a73c410d8302da39b44a7da28
Park Min <ness4208@gmail.com>
2023-08-08 20:29:22 +0900

    feat: 아이템 리소스 Item에 생성

--- Assets/WorkSpace/MinPark/Scripts/Box.cs
+++ Assets/WorkSpace/MinPark/Scripts/Box.cs
@@ -18,7 +18,7 @@ public class Box : MonoBehaviour, IExplosiveReactivable
         item = GetComponentInParent<ItemSetting>();
         // item이 비어있지 않으면 아이템 생성
         if (item.check[index]==1)
-            Instantiate(item.itemArray[item.item[index]], transform.position, Quaternion.Euler(0, 0, 0));
+            PhotonNetwork.Instantiate($"Item/Item[item.item[index]]", transform.position, Quaternion.Euler(0, 0, 0));
         Destroy(gameObject);
     }
 }
