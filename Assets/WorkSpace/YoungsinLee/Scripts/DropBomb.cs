ed2536101c264169e5f5ad3841a38f2ea27ee596
Dongyeon Shin <d.shin0920@gmail.com>
2023-08-09 16:47:08 +0900

    fix: 폭발 유지시간 미적용 버그 해결
    폭발 여파 진행 전 혹은 중에 폭탄을 회수해서 해당 스크립트가 작동하지 못하는 버그를 수정 (폭탄 랜더러만 끄고 여파가 다 진행된 후 회수)

--- Assets/WorkSpace/YoungsinLee/Scripts/DropBomb.cs
+++ Assets/WorkSpace/YoungsinLee/Scripts/DropBomb.cs
@@ -3,121 +3,121 @@ using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class DropBomb : MonoBehaviourPun, IEventListener, IExplosiveReactivable
-{
-    public int tastBomb;
+//public class DropBomb : MonoBehaviourPun, IEventListener, IExplosiveReactivable
+//{
+//    public int tastBomb;
 
 
-    private TestBomb bombPrefab;
-    private TestStat stat;
+//    private TestBomb bombPrefab;
+//    private TestStat stat;
 
-    private LayerMask de;
-    private Vector3 groundDir;
+//    private LayerMask de;
+//    private Vector3 groundDir;
 
-    private int curBomb;
-    private PlayerInput playerInput;
+//    private int curBomb;
+//    private PlayerInput playerInput;
 
-    private void Awake()
-    {
-        if (!photonView.IsMine)
-            Destroy(playerInput);
-        stat = GetComponent<TestStat>();
-        bombPrefab = GameManager.Resource.Load<TestBomb>("Bomb/Bomb");
-    }
+//    private void Awake()
+//    {
+//        if (!photonView.IsMine)
+//            Destroy(playerInput);
+//        stat = GetComponent<TestStat>();
+//        bombPrefab = GameManager.Resource.Load<TestBomb>("Bomb/Bomb");
+//    }
 
-    private void OnFire(InputValue value)
-    {
-         Drop();
-    }
+//    private void OnFire(InputValue value)
+//    {
+//         Drop();
+//    }
 
-    private void OnEnable()
-    {
-        curBomb = stat.Bomb;
-        GameManager.Event.AddListener(EventType.Explode, this);
-    }
+//    private void OnEnable()
+//    {
+//        curBomb = stat.Bomb;
+//        GameManager.Event.AddListener(EventType.Explode, this);
+//    }
 
-    private void CheckBomb()
-    {
-        if (stat.Bomb >= 6)
-            return;
-    }
+//    private void CheckBomb()
+//    {
+//        if (stat.Bomb >= 6)
+//            return;
+//    }
 
-    private Vector3 GroundChack()
-    {
-        RaycastHit hit;
-        Debug.DrawRay(transform.position + 0.3f * Vector3.up, Vector3.down * 0.8f, Color.red);
-        if (Physics.Raycast(transform.position + 0.3f * Vector3.up, Vector3.down, out hit, 0.8f))
-        {
-            if (hit.collider != null && hit.collider.gameObject != null)
-            {
-                groundDir = new Vector3(hit.collider.gameObject.transform.position.x, 0, hit.collider.gameObject.transform.position.z);
-                return groundDir;
-            }
-            else
-            {
-                Debug.Log("밑에 오브젝트가 암것도 없음");
-                return Vector3.zero;
-            }
-        }
-        else
-        {
-            Debug.Log("암것도 안부딪힘");
-            return Vector3.zero;
-        }
-    }
+//    private Vector3 GroundChack()
+//    {
+//        RaycastHit hit;
+//        Debug.DrawRay(transform.position + 0.3f * Vector3.up, Vector3.down * 0.8f, Color.red);
+//        if (Physics.Raycast(transform.position + 0.3f * Vector3.up, Vector3.down, out hit, 0.8f))
+//        {
+//            if (hit.collider != null && hit.collider.gameObject != null)
+//            {
+//                groundDir = new Vector3(hit.collider.gameObject.transform.position.x, 0, hit.collider.gameObject.transform.position.z);
+//                return groundDir;
+//            }
+//            else
+//            {
+//                Debug.Log("밑에 오브젝트가 암것도 없음");
+//                return Vector3.zero;
+//            }
+//        }
+//        else
+//        {
+//            Debug.Log("암것도 안부딪힘");
+//            return Vector3.zero;
+//        }
+//    }
 
-    private void Drop()
-    {
-        if (curBomb == 0)
-            return;
-        if(curBomb > stat.Bomb)
-            curBomb = stat.Bomb;
-        photonView.RPC("RequestCreateBomb", RpcTarget.MasterClient, transform.position, transform.rotation);
-        //GameManager.Resource.Instantiate(bomb, GroundChack(), transform.rotation);
-        curBomb--;
-        // 네트워크 식
-    }
+//    private void Drop()
+//    {
+//        if (curBomb == 0)
+//            return;
+//        if(curBomb > stat.Bomb)
+//            curBomb = stat.Bomb;
+//        photonView.RPC("RequestCreateBomb", RpcTarget.MasterClient, transform.position, transform.rotation);
+//        //GameManager.Resource.Instantiate(bomb, GroundChack(), transform.rotation);
+//        curBomb--;
+//        // 네트워크 식
+//    }
 
-    [PunRPC]
-    private void RequestCreateBomb(Vector3 position, Quaternion rotation, PhotonMessageInfo info)
-    {
-        float sentTime = (float)info.SentServerTime;
-        // 반장이 직접 판단하지 않고 서버를 한번 걸치는 식으로 서버를 통해 판단하게 설정(공평성을 위해)
-        photonView.RPC("ResultCreateBomb", RpcTarget.AllViaServer, position, rotation, sentTime, info.Sender);
+//    [PunRPC]
+//    private void RequestCreateBomb(Vector3 position, Quaternion rotation, PhotonMessageInfo info)
+//    {
+//        float sentTime = (float)info.SentServerTime;
+//        // 반장이 직접 판단하지 않고 서버를 한번 걸치는 식으로 서버를 통해 판단하게 설정(공평성을 위해)
+//        photonView.RPC("ResultCreateBomb", RpcTarget.AllViaServer, position, rotation, sentTime, info.Sender);
     
-    }
+//    }
 
 
-    [PunRPC]
-    private void ResultCreateBomb(Vector3 position, Quaternion rotation, float sentTime, Player player)
-    {
-        float lag = (float)(PhotonNetwork.Time - sentTime);
-        position = GroundChack();
-        Instantiate(bombPrefab, position, rotation); // 값을 정해서 보내면 더욱 정확한 타이밍을 맞출수있음
-        bombPrefab.SetPlayer(player);
-        Debug.Log($"{photonView.Owner.NickName}발싸!");
-    }
+//    [PunRPC]
+//    private void ResultCreateBomb(Vector3 position, Quaternion rotation, float sentTime, Player player)
+//    {
+//        float lag = (float)(PhotonNetwork.Time - sentTime);
+//        position = GroundChack();
+//        Instantiate(bombPrefab, position, rotation); // 값을 정해서 보내면 더욱 정확한 타이밍을 맞출수있음
+//        bombPrefab.SetPlayer(player);
+//        Debug.Log($"{photonView.Owner.NickName}발싸!");
+//    }
 
     
 
-    // 폭탄 폭발시 갯수 추가
-    public void OnEvent(EventType eventType, Component Sender, object Param = null)
-    {
-        if(EventType.Explode == eventType)
-        {
-            curBomb++;
-        }
-    }
+//    // 폭탄 폭발시 갯수 추가
+//    public void OnEvent(EventType eventType, Component Sender, object Param = null)
+//    {
+//        if(EventType.Explode == eventType)
+//        {
+//            curBomb++;
+//        }
+//    }
 
-    public void ExplosiveReact()
-    {
-        // 죽는 애니메이션 실행
+//    public void ExplosiveReact()
+//    {
+//        // 죽는 애니메이션 실행
         
-    }
+//    }
 
-    public void OnExitButtom()
-    {
-        PhotonNetwork.LeaveRoom();
-        PhotonNetwork.JoinLobby();
-    }
-}
+//    public void OnExitButtom()
+//    {
+//        PhotonNetwork.LeaveRoom();
+//        PhotonNetwork.JoinLobby();
+//    }
+//}
